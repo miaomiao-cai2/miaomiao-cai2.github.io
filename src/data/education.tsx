46feaafe2f0d9a5336c062d1e0f73612d8040900
miaomiao cai <56181838+miaomiao-cai2@users.noreply.github.com>
2026-09-02 13:11:49 +0800

    Update education.tsx

--- src/data/education.tsx
+++ src/data/education.tsx
@@ -1,7 +1,7 @@
 export type EducationConfig = typeof educationData;
 
 import NUS from "@/assets/img/nus.svg";
-import HFUT from "@/assets/img/hfut.png";
+import HFUT from "@/assets/img/hfut.svg";
 import THU from "@/assets/img/thu.png";
 
 export const educationData = [
