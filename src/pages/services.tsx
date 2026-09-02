e498738fb61d986d8d6336fc10b8d9509e2c2b11
miaomiao cai <56181838+miaomiao-cai2@users.noreply.github.com>
2026-09-02 13:16:38 +0800

    Update services.tsx

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -6,7 +6,8 @@ export default function ServicesPage() {
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-2xl w-full justify-center">
           <div className="text-xl font-bold mb-4">Services</div>
-          <div className="text-default-500">Coming soon.</div>
+          <div className="text-default-500">Reviewer for KDD, MM, AAAI, TKDE, TOIS, SIGIR, WWW, SIGIR-AP.</div>
+         
         </div>
       </section>
     </DefaultLayout>
